bff1fda03fa53635a2731acb2f64ae4158aef0c2
geethsowri <nainalageethsowri@gmail.com>
2025-02-09 21:41:19 +0530

    added some problems

--- Introductory Problems/10-trailing-zeros.cpp
+++ Introductory Problems/10-trailing-zeros.cpp
@@ -1,14 +1,14 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 int main() {
-    int t;
-    cin >> t; 
-    while(t--) {
-        int a, b;
-        cin >> a >> b;
-        if (((2 * a - b) % 3 != 0) || ((2 * a - b) < 0) || ((2 * b - a) % 3 != 0) || ((2 * b - a) < 0)) 
-            cout << "NO";
-        else 
-            cout << "YES";
+    long long n;
+    cin >> n;
+    long long cur = 5;
+    long long res = 0;
+    while(cur <= n) {
+        res += (n / cur);
+        cur *= 5;
     }
+    cout << res;
+    return 0;
 }
---
+++ Introductory Problems/11-coin-piles.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t; 
+    while(t--) {
+        int a, b;
+        cin >> a >> b;
+        if (((2 * a - b) % 3 != 0) || ((2 * a - b) < 0) || ((2 * b - a) % 3 != 0) || ((2 * b - a) < 0)) 
+            cout << "NO\n";
+        else 
+            cout << "YES\n";
+    }
+
+}
